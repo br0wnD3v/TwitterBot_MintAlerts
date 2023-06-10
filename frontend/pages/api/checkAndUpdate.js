d64e04b31c732c47ee03eb02e18abd66c2487f96
br0wnD3v <bothereum@protonmail.com>
2023-06-11 04:12:13 +0530

    Added the twitter handle.

--- frontend/pages/api/checkAndUpdate.js
+++ frontend/pages/api/checkAndUpdate.js
@@ -1,5 +1,15 @@
+require("dotenv").config();
+
 import { ethers } from "ethers";
 import { contractABI, contractAddress } from "../../constants/info.js";
+import Twit from "twit";
+
+const T = new Twit({
+  consumer_key: process.env.TWT_API_KEY,
+  consumer_secret: process.env.TWT_API_SECRET,
+  access_token: process.env.TWT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWT_ACCESS_SECRET,
+});
 
 export default async function handler(req, res) {
   if (req.method === "UPDATE") {
@@ -17,9 +27,24 @@ export default async function handler(req, res) {
       );
 
       // Set up the event listener
-      contract.on("ChallengeSolved", (receiver, sender, twitterHandle) => {
-        console.log("Twitter:", twitterHandle);
-      });
+      contract.on(
+        "ChallengeSolved",
+        async (receiver, sender, twitterHandle) => {
+          console.log("Twitter Handle:", twitterHandle);
+          const tweetContent = `Congratulations to @${twitterHandle} as they are being rewarded an exclusive NFT on @Arbitrum which was made available after completing a series of challenges. Special thanks to @CyfrinAudits for making this happen! :)`;
+
+          try {
+            const response = await T.post("statuses/update", {
+              status: tweetContent,
+            });
+
+            res.status(200).json({ message: "Tweet posted successfully!" });
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Failed to post tweet." });
+          }
+        }
+      );
 
       // Start listening for events
       contract.start();
